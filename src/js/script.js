76e877cdf0f27a6a0bb1e951b13dd3ec103aba15
Qsimple <562713990@qq.com>
2017-12-08 18:52:29 +0800

    添加音频播放器

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,34 @@
 jQuery(document).ready(function($) {
 	var imgFilePrefix = "../resources/doreen_pronounce_skills/发音技巧"
 	var imgFileSuffix = ".jpg"
+	var audioFileSuffix = ".mp3"
 	var currentIndex = 1
 	var maxIndex = 20;
 
 	$("button#next").click(function(event) {
+		// change img
 		var nextImg = imgFilePrefix + ++currentIndex + imgFileSuffix
+		var nextAudio = imgFilePrefix + currentIndex + audioFileSuffix
 		if (currentIndex > maxIndex) {
 			alert("No more pictures!!!")
 			currentIndex--
 			return
 		}
+		// change img
 		$("img#contentImg").attr({
 			"src": nextImg,
 		});
+		// change audio
+		$("source#audio_src").attr({
+			"src": nextAudio,
+		});
+		// reload audio
+		$("audio#pronounce_audio")[0].load();
 	});
 
 	$("button#pre").click(function(event) {
 		var preImg = imgFilePrefix + --currentIndex + imgFileSuffix
+		var preAudio = imgFilePrefix + currentIndex + audioFileSuffix
 		if (currentIndex < 1) {
 			alert("This is the first picture!!!")
 			currentIndex++
@@ -26,5 +37,11 @@ jQuery(document).ready(function($) {
 		$("img#contentImg").attr({
 			"src": preImg,
 		});
+		// change audio
+		$("source#audio_src").attr({
+			"src": preAudio,
+		});
+		// reload audio
+		$("audio#pronounce_audio")[0].load();
 	});
 });
